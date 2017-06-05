f5824471bfb394051f497cfa8313a1637085ffbb
nwatarai <watarai.n.aa@m.titech.ac.jp>
2017-06-05 11:41:24 +0900

    revise function added

--- gamma_estimation.py
+++ gamma_estimation.py
@@ -5,7 +5,7 @@ from scipy.special import gamma, digamma
 import sys
 
 class gamma_mixture(object):
-    def __init__(self, n_component, iter_max=1000):
+    def __init__(self, n_component=2, iter_max=1000, scale=True):
         self.n_component = n_component
         self.weights = None
         self.alpha = None
@@ -14,28 +14,34 @@ class gamma_mixture(object):
         self.BIC_transition = []
         self.iter_max = iter_max
         self.weight_threshold = None
+        self.scale = scale
+        self.scaled_X = None
 
     def fit(self, X):
-        self.weight_threshold = 1 / float(X.shape[0])
-        self.ndim = X.shape[1] 
-        self.weights = np.ones(self.n_component) / float(self.n_component)
-        self.alpha = np.random.uniform(0.1, 10.0, (self.ndim, self.n_component))
-        self.lamda = np.random.uniform(0.1, 10.0, (self.ndim, self.n_component))
+        X = X.astype(np.float64)
+        if self.scale:
+            X = self.scaling(X)
+        if not X == None:
+            self.weight_threshold = 1 / float(X.shape[0])
+            self.ndim = X.shape[1] 
+            self.weights = np.ones(self.n_component) / float(self.n_component)
+            self.alpha = np.random.uniform(0.1, 10.0, (self.ndim, self.n_component))
+            self.lamda = np.random.uniform(0.1, 10.0, (self.ndim, self.n_component))
 
-        step_size = np.power(np.ones(self.iter_max) / (np.arange(self.iter_max) + 1), 0.2)
+            step_size = np.power(np.ones(self.iter_max) / (np.arange(self.iter_max) + 1), 0.2)
 
-        for i in range(self.iter_max):
-            params = np.concatenate((self.weights.ravel(), self.alpha.ravel(), self.lamda.ravel()), axis=1)
-            resps = self.expectation(X)
-            self.maximization(X, resps, step_size[i])
-            estimated = np.concatenate((self.weights.ravel(), self.alpha.ravel(), self.lamda.ravel()), axis=1)
-            self.cal_BIC(X)
-            if np.allclose(params, estimated):
-                print "parameters have converged at step_%i" %(i + 1)
-                break
-            self.degenerate_param()
-        else:
-            print "parameters may not have converged in %i times" %self.iter_max
+            for i in range(self.iter_max):
+                params = np.ravel((self.weights.ravel(), self.alpha.ravel(), self.lamda.ravel()))
+                resps = self.expectation(X)
+                self.maximization(X, resps, step_size[i])
+                estimated = np.ravel((self.weights.ravel(), self.alpha.ravel(), self.lamda.ravel()))
+                self.cal_BIC(X)
+                if np.allclose(params, estimated):
+                    print "parameters have converged at step_%i" %(i + 1)
+                    break
+                self.degenerate_param()
+            else:
+                print "parameters may not have converged in %i times" %self.iter_max
 
     def gamma_pdf(self, X):
         return self.lamda ** self.alpha * X ** (self.alpha - 1) * np.exp(- self.lamda * X) / gamma(self.alpha)
@@ -62,6 +68,18 @@ class gamma_mixture(object):
         self.alpha = self.alpha[:,~index]
         self.lamda = self.lamda[:,~index]
         self.weights /= self.weights.sum()
+
+    def scaling(self, X):
+        if X.sum() != 0:
+            _X = X / X.max()
+            _X[_X==0] = np.inf
+            _min = np.min(_X)
+            _X[np.isinf(_X)] = 0.0
+            assert _min != 0
+            _X += _min / 2 
+            self.scaled_X = _X
+            return _X 
+        return None
 
 def pseudo_digamma(x):
     _x = x - 0.5
--- respective_gamma.py
+++ respective_gamma.py
@@ -1,28 +1,42 @@
-import np
+import numpy as np
 from matplotlib import pyplot
 import sys
-import panda as pd
+import pandas as pd
 
 from gamma_estimation import gamma_mixture, Gamma, BIC
 
+def scaling(X):
+    if X.sum() != 0:
+        _X = X / X.sum(axis=0)
+        assert _X.shape == X.shape
+        return _X
+    return np.zeros(X.shape)
+
 if __name__ == '__main__':
-    mtx = pd.read_csv(sys.argv[1], header=0, index_col=0, sep="\t")
-    values = mtx.values
-    gm = gamma_mixture(2)
+    mtx = pd.read_csv(sys.argv[1], header=0, index_col=0, sep="\t", comment="#")
+    values = mtx.values.astype(np.float64)
+    gm = gamma_mixture(n_component=2, scale=True)
 
-
-    for i in range(1):
-        gm.fit(values[i].T)
+    for i in range(100):
+        v = np.array([values[i]])
+        gm.fit(v.T)
         weights = gm.weights
         alpha = gm.alpha
         lamda = gm.lamda
 
-        seq = np.arange(0, 50, 0.02).astype(np.float64)
-        print BIC(values[i].T)
-        for i in range(degenerate_n_component):
-            line = Gamma(seq, a[0][i], b[0][i], w[i])
+        """
+        scaled_X = gm.scaled_X
+        seq = np.arange(0, 1, 0.02).astype(np.float64)
+        S = np.zeros(seq.shape)
+        print weights
+        print lamda
+        print alpha
+        print BIC(scaled_X, alpha, lamda, weights)
+        for i in range(weights.shape[0]):
+            line = Gamma(seq, alpha[0][i], lamda[0][i], weights[i])
             pyplot.plot(seq, line, linewidth = 1.0, color="g")
         S += line
         pyplot.plot(seq, S, linewidth=4.0, color="r")
-        pyplot.hist(data.T, normed=True, bins=10)
-        pyplot.show()
+        pyplot.hist(scaled_X, normed=True, bins=10)"""
+        if weights.shape[0] == 2:
+            print mtx.index[i]
